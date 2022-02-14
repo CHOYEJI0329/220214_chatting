5f1990cd71d71476f59042d3fbad85dae0c2acf3
choyeji0329 <choyeji0329@gmail.com>
2022-02-14 17:59:10 +0900

    [20220214] initial & login page css COMMIT

---
+++ src/main/java/com/project/first/ticket/config/WebSecurityConfig.java
@@ -0,0 +1,63 @@
+package com.project.first.ticket.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable() // 스프링 시큐리티 내에 있는 csrf 보안 기술을 없애는거라 보안상 안좋지만 현재는 테스트 용이므로 없앴음.
+                .authorizeRequests()
+                    .antMatchers("/account/register", "/css/**", "/js/**", "/img/**").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                .formLogin()
+                    .loginPage("/account/login")
+                    .permitAll()
+                    .and()
+                .logout()
+                    .permitAll();
+    }
+
+    /*
+    Authentication 로그인
+    Authroization  권한
+    */
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+//                .passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery("select username,password,enabled "   // 인증처리
+                        + "from user "
+                        + "where username = ?")
+                .authoritiesByUsernameQuery("select u.username, r.name "    // 권한처리
+                        + "from user_role ur inner join user u on ur.user_id = u.id "
+                        + "inner join role r on ur.role_id = r.id "
+                        + "where u.username = ?");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
